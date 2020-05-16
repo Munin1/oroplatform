6b296cf25312b9a350366c35fe47acdc34c1b174
Denny Mattern <mattern@posteo.de>
2020-05-16 23:08:19 +0200

    update mechanism

--- oro/docker-entrypoint.sh
+++ oro/docker-entrypoint.sh
@@ -16,19 +16,49 @@ then
     cp -r /opt/oroapp /var/www/html
     cd /var/www/html/oroapp
 
-    php ./bin/console oro:install --env=prod --timeout=5000 --no-debug --application-url="http://$APP_HOST/" --organization-name="$ORGNAME" --user-name="$APP_USER" --user-email="$APP_USER_EMAIL" --user-firstname="$USER_FIRST_NAME" --user-lastname="$USER_LAST_NAME" --user-password="$APP_PASSWORD" --sample-data=$APP_LOAD_DEMO_DATA --language=de --formatting-code=de_DE
+    php ./bin/console oro:install --env=prod --timeout=5000 --no-debug --application-url="http://$APP_HOST/" --organization-name="$ORGNAME" --user-name="$APP_USER" --user-email="$APP_USER_EMAIL" --user-firstname="$USER_FIRST_NAME" --user-lastname="$USER_LAST_NAME" --user-password="$APP_PASSWORD" --sample-data=$APP_LOAD_DEMO_DATA --language=de --formatting-code=de_DE --verbose
 
     # fix permissions
     setfacl -b -R ./
     find . -type f -exec chmod 0644 {} + -o -type d -exec chmod 0755 {} +
-#    chown -R www-data:www-data ./var/{sessions,attachment,cache,import_export,logs}
-#    chown -R www-data:www-data ./public/{media,uploads,js}
     chown -R www-data:www-data /var/www/html/oroapp/
 
     RUNMODE="run"
 
 fi # End install
 
+
+
+if [ "$RUNMODE" = "update" ]; then
+
+    sleep 30
+    echo "Entering update mode"
+
+    cp -r /opt/oroapp /var/www/html
+    cd /var/www/html/oroapp
+    
+    # clear cache
+    rm -rf var/cache/prod
+    
+    # update code
+    php ./bin/console oro:platform:update --env=prod --force --verbose
+    
+    # update parameters.yml
+    sed -i "s/    installed:              ~/    installed:              '$(date --iso-8601=seconds)'/g" config/parameters.yml
+    sed -i "s/~/null/g" config/parameters.yml
+    
+    # clear cache
+    php ./bin/console cache:clear --env=prod
+    php ./bin/console cache:warmup --env=prod
+
+
+    # fix permissions
+    setfacl -b -R ./
+    find . -type f -exec chmod 0644 {} + -o -type d -exec chmod 0755 {} +
+    chown -R www-data:www-data /var/www/html/oroapp/
+
+    RUNMODE="run"
+fi
 
 
 if [ "$RUNMODE" = "run" ];
